6342553815008625b553c17387e372e450b3d484
John Doe <javatechisland@gmail.com>
2020-10-28 16:52:28 -0700

    telegram first commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDxYnXats09rhxwtWSotEzWTmgfCdgy7U0",
+    authDomain: "telegram-29077.firebaseapp.com",
+    databaseURL: "https://telegram-29077.firebaseio.com",
+    projectId: "telegram-29077",
+    storageBucket: "telegram-29077.appspot.com",
+    messagingSenderId: "984191568575",
+    appId: "1:984191568575:web:4c369efe9ed9933749940a",
+    measurementId: "G-C252FYE2HR"
+  };
