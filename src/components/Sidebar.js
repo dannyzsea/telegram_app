e2f0f4f4803f76ced9b38c292581b6098a44649c
John Doe <javatechisland@gmail.com>
2020-10-28 17:14:05 -0700

    searchicon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import SearchIcon from "@material-ui/icons/Search";
 const Sidebar = () => {
     return (
         <div className="sidebar">
          <div className="sidebar__header">
-
-         </div>
-        </div>
+<div className="sidebar__search">
+<SearchIcon className="sidebar__searchIcon"/>
+    <input placeholder="Search" className="sidebar__input"/>
+</div>
+</div>
+<div className="sidebar__threads"></div>
+<div className="sidebar__bottom"></div>
+</div>
     )
 }
 
